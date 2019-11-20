2a252f4b9e2469f3e831b6fcae67ab6c4368e413
ibraim-Ajarmeh <ajarmeh1997@gmail.com>
2019-11-20 16:36:22 +0200

    wtarting with the event api

---
+++ index.js
@@ -0,0 +1,119 @@
+`use strict`;
+
+require('dotenv').config();
+const express = require('express')
+const cors = require('cors')
+const superagent = require('superagent');
+
+const PORT = process.env.PORT || 3000;
+
+const server = express();
+
+server.use(cors());
+
+server.get('/', (request, response) => {
+    response.status(200).send('Hello everyone')
+})
+
+server.get('/location', locationHandler);
+server.get('/weather', weatherHandler);
+server.get('/event', eventHandler);
+
+
+function locationHandler(req, res) {
+    getLocation(req.query.data)
+        .then(locationData => res.status(200).json(locationData));
+}
+
+
+function getLocation(city) {
+
+    const url = `https://maps.googleapis.com/maps/api/geocode/json?address=${city}&key=${process.env.GEOCODE_API_KEY}`
+    console.log('url', url);
+    
+    return superagent.get(url)
+        .then(data => {
+            return new Location(city, data.body);
+        })
+
+}
+
+function Location(city, data) {
+    this.search_query = city;
+    this.formatted_query = data.results[0].formatted_address;
+    this.latitude = data.results[0].geometry.location.lat;
+    this.longitude = data.results[0].geometry.location.lng;
+
+}
+
+
+// ---- Weather section ---- \\
+
+function weatherHandler(req, res) {
+    // Query String = ?a=b&c=d
+    getWeather(req.query.data)
+        .then(weatherData => res.status(200).json(weatherData));
+
+}
+
+function getWeather(query) {
+    const url = `https://api.darksky.net/forecast/${process.env.DARKSKY_API_KEY}/${query.latitude},${query.longitude}`;
+    console.log('url',url);
+    return superagent.get(url)
+        .then(data => {
+            let weather = data.body;
+            return weather.daily.data.map((day) => {
+                return new Weather(day);
+            });
+        });
+}
+
+function Weather(day) {
+    this.forecast = day.summary;
+    this.time = new Date(day.time * 1000).toDateString();
+}
+
+// ---- Event section ---- \\
+
+function eventHandler(req, res) {
+    // Query String = ?a=b&c=d
+    getEvent(req.query.data)
+        .then(eventData => res.status(200).json(eventData));
+
+}
+
+function getEvent(query) {
+    const url = `http://api.eventful.com/json/events/search?app_key=${process.env.EVENT_API_KEY}&location=${query.formatted_query}`;
+    console.log('url',url);
+    return superagent.get(url)
+        .then(data => {
+            console.log('data', data);
+            
+            let event = data.body;
+            return event.daily.data.map((day) => {
+                return new Event(day);
+            });
+        });
+}
+
+function Event(day) {
+    this.forecast = day.summary;
+    this.time = new Date(day.time * 1000).toDateString();
+}
+
+
+server.get('/foo', (request, response) => {
+    throw new Error('ops');
+})
+
+server.use('*', (request, response) => {
+    response.status(404).send('Not Found')
+})
+
+server.use((error, request, response) => {
+    response.status(500).send(error)
+})
+
+
+
+server.listen(PORT, () => console.log(`app listening on ${PORT}`))
